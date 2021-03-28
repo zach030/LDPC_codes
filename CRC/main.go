ef94f276cd45db0e97b9c393148ae0ee83dd1462
Zach <zachchou016@gmail.com>
2021-03-28 18:55:11 +0800

    [add] implement crc

--- CRC/main.go
+++ CRC/main.go
@@ -1,1 +1,50 @@
-package CRC
+package main
+
+import "fmt"
+
+const (
+	n          = 6
+	k          = 4
+	POLYNOMIAL = 0X9800 // 生成多项式 (1001 1000 0000 0000)
+	DATAFRAME  = 0X002B // 信息位    (0000 0000 0010 1011)
+)
+
+func main() {
+	ch := make(chan uint16, 1)
+	// 信息位左移4位: 101011 0000
+	dataFrame := fillZero(DATAFRAME, k)
+	// 生成多项式除以信息位，得到FCS序列 0100
+	fcs := crcDivide(POLYNOMIAL, dataFrame)
+	// 生成消息帧: 101011 0100 发送到管道
+	ch <- getDataFrame(dataFrame, fcs)
+	// 从管道接收并解析消息帧
+	res := decodeDataFrame(POLYNOMIAL, <-ch)
+	fmt.Printf("crc result is: %d", res)
+}
+
+func fillZero(data uint16, size int) uint16 {
+	return data << size
+}
+
+func getDataFrame(data, fcs uint16) uint16 {
+	return data | (fcs & 0X0F)
+}
+
+func crcDivide(check, data uint16) uint16 {
+	i := uint16(0)
+	/* 初始化，余数=原始数据 */
+	remainder := data
+	/* 从最高位开始依次计算  */
+	for i = 0; i < 16; i++ {
+		if remainder&0X8000 == 1 {
+			remainder ^= check
+		}
+		remainder = remainder << 1
+	}
+	/* 返回计算的CRC码 */
+	return remainder & 0X000F
+}
+
+func decodeDataFrame(check, data uint16) uint16 {
+	return crcDivide(check, data)
+}
